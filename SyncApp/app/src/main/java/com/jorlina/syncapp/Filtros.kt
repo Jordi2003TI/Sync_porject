317db568a12b44c4d6284f2c76e0d440fa91618d
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-11-21 19:43:01 +0100

    agregar elemeto a recycle view

---
+++ SyncApp/app/src/main/java/com/jorlina/syncapp/Filtros.kt
@@ -0,0 +1,15 @@
+package com.jorlina.syncapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Filtros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtros)
+
+    }
+}
