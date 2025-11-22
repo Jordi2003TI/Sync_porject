80c9f56293ca09451b946878e5edef151a188f7e
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-11-22 11:54:09 +0100

    arregalar porque no cargaba

--- SyncApp/app/src/main/java/com/jorlina/syncapp/MainActivity.kt
+++ SyncApp/app/src/main/java/com/jorlina/syncapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jorlina.syncapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,9 +11,29 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents();
+        initListeners();
+        initUI()
+
+    }
+
+    private fun initComponents(){
+        btLogin = findViewById(R.id.btLogin)
+    }
+
+    private fun initListeners(){
+        btLogin.setOnClickListener {
+            val intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initUI(){
 
     }
 }
