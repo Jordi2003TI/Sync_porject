28c00bbf3f9898e00aad1b84d9f4e73cb63f3821
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-11-22 12:28:33 +0100

    perfil

---
+++ SyncApp/app/src/main/java/com/jorlina/syncapp/Perfil.kt
@@ -0,0 +1,30 @@
+package com.jorlina.syncapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Perfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+        initComponents();
+        initListeners();
+        initUI()
+
+    }
+
+    private fun initComponents(){
+
+    }
+
+    private fun initListeners(){
+
+    }
+
+    private fun initUI(){
+
+    }
+}
