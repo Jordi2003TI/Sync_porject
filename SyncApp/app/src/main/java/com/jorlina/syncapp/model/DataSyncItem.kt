d5f7b0c9300bb9c3d29df9b17116d72f00ff6a23
JordiIbanezMasip <jordimasip2003@gmail.com>
2026-01-09 18:48:46 +0100

    cambio recycle view foto perfil

--- SyncApp/app/src/main/java/com/jorlina/syncapp/model/DataSyncItem.kt
+++ SyncApp/app/src/main/java/com/jorlina/syncapp/model/DataSyncItem.kt
@@ -5,11 +5,10 @@ import com.jorlina.syncapp.R
 object DataSyncItem{
 
     val item: List<SyncItem> = listOf(
-        SyncItem("Ejemplo 1", "ejemplo descripcion", 2, true, R.drawable.favorite_icon, R.drawable.perfil_layaut_image, "Biologia General"),
-        SyncItem("Ejemplo 2", "ejemplo descripcion", 5, true, R.drawable.favorite_icon, R.drawable.perfil_layaut_image, "Calculo"),
-        SyncItem("Ejemplo 3", "ejemplo descripcion", 1, false, R.drawable.favorite_icon, R.drawable.perfil_layaut_image, "Quimica Organica"),
-        SyncItem("Ejemplo 4", "ejemplo descripcion", 4, true, R.drawable.favorite_icon, R.drawable.perfil_layaut_image, "Calculo"),
-        SyncItem("Ejemplo 5", "ejemplo descripcion", 3, false, R.drawable.favorite_icon, R.drawable.perfil_layaut_image,"Fundamentos de la programacion")
-
+        SyncItem("Ejemplo 1", "ejemplo descripcion", 2, true, R.drawable.favorite_icon, R.drawable.user_icon_image, "Biologia General"),
+        SyncItem("Ejemplo 2", "ejemplo descripcion", 5, true, R.drawable.favorite_icon, R.drawable.user_icon_image, "Calculo"),
+        SyncItem("Ejemplo 3", "ejemplo descripcion", 1, false, R.drawable.favorite_icon, R.drawable.user_icon_image, "Quimica Organica"),
+        SyncItem("Ejemplo 4", "ejemplo descripcion", 4, true, R.drawable.favorite_icon, R.drawable.user_icon_image, "Calculo"),
+        SyncItem("Ejemplo 5", "ejemplo descripcion", 3, false, R.drawable.favorite_icon, R.drawable.user_icon_image,"Fundamentos de la programacion")
     )
-}
+} 
