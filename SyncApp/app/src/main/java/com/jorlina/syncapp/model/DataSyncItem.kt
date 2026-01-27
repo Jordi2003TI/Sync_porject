f84e2aa3f8606256eaa2faaad45dea46848064a1
Carolw3 <carolina.melguizos3@gmail.com>
2026-01-27 19:18:43 +0100

    pequeño retoque en la visivilizacion de la descripcion en el item del recicler y en el logo de la splashscreen

--- SyncApp/app/src/main/java/com/jorlina/syncapp/model/DataSyncItem.kt
+++ SyncApp/app/src/main/java/com/jorlina/syncapp/model/DataSyncItem.kt
@@ -5,7 +5,7 @@ import com.jorlina.syncapp.R
 object DataSyncItem{
 
     val item: List<SyncItem> = listOf(
-        SyncItem("Ejemplo 1", "ejemplo descripcion", 2, true, R.drawable.docs_icon, R.drawable.user_icon_image, "Biologia General"),
+        SyncItem("Ejemplo 1", "ejemplo descripcion muy muy larga para ver como se ve", 2, true, R.drawable.docs_icon, R.drawable.user_icon_image, "Biologia General"),
         SyncItem("Ejemplo 2", "ejemplo descripcion", 5, true, R.drawable.docs_icon, R.drawable.user_icon_image, "Calculo"),
         SyncItem("Ejemplo 3", "ejemplo descripcion", 1, false, R.drawable.docs_icon, R.drawable.user_icon_image, "Quimica Organica"),
         SyncItem("Ejemplo 4", "ejemplo descripcion", 4, true, R.drawable.docs_icon, R.drawable.user_icon_image, "Calculo"),
