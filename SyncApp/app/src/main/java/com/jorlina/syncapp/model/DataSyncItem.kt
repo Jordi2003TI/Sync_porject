83af5b2628e1d809a2b7876488a43cc215bdf995
Carolw3 <carolina.melguizos3@gmail.com>
2026-02-09 19:05:43 +0100

    .

--- SyncApp/app/src/main/java/com/jorlina/syncapp/model/DataSyncItem.kt
+++ SyncApp/app/src/main/java/com/jorlina/syncapp/model/DataSyncItem.kt
@@ -22,9 +22,55 @@ object DataSyncItem{
 
         SyncItem(
             2,
-            1002,"Ejemplo 2", "ejemplo descripcion", 5, true, R.drawable.docs_icon, R.drawable.user_icon_image, "Calculo", now, now ),
-        SyncItem(3, 1003,"Ejemplo 3", "ejemplo descripcion", 1, false, R.drawable.docs_icon, R.drawable.user_icon_image, "Quimica Organica", now, now ),
-        SyncItem(4, 1004,"Ejemplo 4", "ejemplo descripcion", 4, true, R.drawable.docs_icon, R.drawable.user_icon_image, "Calculo", now, now ),
-        SyncItem(5, 1005,"Ejemplo 5", "ejemplo descripcion", 3, false, R.drawable.docs_icon, R.drawable.user_icon_image,"Fundamentos de la programacion", now, now )
+            1002,
+            "Ejemplo 2",
+            "ejemplo descripcion",
+            5,
+            true,
+            R.drawable.docs_icon,
+            R.drawable.user_icon_image,
+            "Calculo",
+            now,
+            now ),
+
+
+        SyncItem(
+            3,
+            1003,
+            "Ejemplo 3",
+            "ejemplo descripcion",
+            1,
+            false,
+            R.drawable.docs_icon,
+            R.drawable.user_icon_image,
+            "Quimica Organica",
+            now,
+            now ),
+
+
+        SyncItem(
+            4,
+            1004,
+            "Ejemplo 4",
+            "ejemplo descripcion",
+            4, true,
+            R.drawable.docs_icon,
+            R.drawable.user_icon_image,
+            "Calculo",
+            now,
+            now ),
+
+
+        SyncItem(
+            5,
+            1005,
+            "Ejemplo 5",
+            "ejemplo descripcion",
+            3, false,
+            R.drawable.docs_icon,
+            R.drawable.user_icon_image,
+            "Fundamentos de la programacion",
+            now,
+            now )
     )
 } 
