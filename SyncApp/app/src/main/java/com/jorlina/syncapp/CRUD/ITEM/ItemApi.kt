3460048683e58c3cfdc5d79ddba255c4d86c7334
JordiIbanezMasip <jordimasip2003@gmail.com>
2026-02-12 18:56:52 +0100

    mas cambios de retrofit

---
+++ SyncApp/app/src/main/java/com/jorlina/syncapp/CRUD/ITEM/ItemApi.kt
@@ -0,0 +1,65 @@
+package com.jorlina.syncapp.CRUD.ITEM
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import java.security.cert.X509Certificate
+import javax.net.ssl.X509TrustManager
+
+class ItemApi {
+    companion object {
+        private var mItemAPI: ItemService? = null
+
+        @Synchronized
+        fun API(): ItemService {
+            if (mItemAPI == null) {
+
+                val gsondateformat = GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                    .create()
+
+                // Client HTTP insegur (només per desenvolupament)
+                val unsafeOkHttpClient = getUnsafeOkHttpClient()
+
+                mItemAPI = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create(gsondateformat))
+                    .baseUrl("https://oracleitic.mooo.com/")
+                    .client(unsafeOkHttpClient) // Afegeix el client
+                    .build()
+                    .create(ItemService::class.java)
+            }
+            return mItemAPI!!
+        }
+
+        private fun getUnsafeOkHttpClient(): OkHttpClient {
+            try {
+                // Crea un trust manager que NO valida certificats
+                val trustAllCerts = arrayOf<TrustManager>(
+                    object : X509TrustManager {
+                        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+                        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+                        override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                    }
+                )
+
+                // Instal·la el trust manager
+                val sslContext = SSLContext.getInstance("SSL")
+                sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+                val sslSocketFactory = sslContext.socketFactory
+
+                return OkHttpClient.Builder()
+                    .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                    .hostnameVerifier { _, _ -> true } // Accepta qualsevol hostname
+                    .build()
+
+            } catch (e: Exception) {
+                throw RuntimeException(e)
+            }
+        }
+    }
+}
+
+annotation class ItemService
