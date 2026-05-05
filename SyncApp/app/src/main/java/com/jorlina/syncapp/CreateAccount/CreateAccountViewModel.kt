a4900e3424613718ed3c5a26e9d39c44c1aaa01e
JordiIbanezMasip <jordimasip2003@gmail.com>
2026-05-05 19:32:43 +0200

    viewModel

--- SyncApp/app/src/main/java/com/jorlina/syncapp/CreateAccount/CreateAccountViewModel.kt
+++ SyncApp/app/src/main/java/com/jorlina/syncapp/CreateAccount/CreateAccountViewModel.kt
@@ -18,33 +18,34 @@ class CreateAccountViewModel : ViewModel() {
         password: String,
         repeatPassword: String
     ) {
-        //Test campos vacios
+
         if (username.isBlank()) {
             _errorMessage.value = "Usuario obligatorio"
             return
         }
-        if (email.isBlank()) {
-            _errorMessage.value = "Email obligatorio"
-            return
-        }
-        if (password.isBlank()) {
-            _errorMessage.value = "Contaraseña obligatoria"
-            return
-        }
-        if (repeatPassword.isBlank()) {
-            _errorMessage.value = "Debes confirmar la contraseña"
+
+        if (username.length < 3) {
+            _errorMessage.value = "Usuario demasiado corto"
             return
         }
 
+        if (username.length > 50) {
+            errorMessage.value = "Usuario demasiado largo"
+            return
+        }
 
-        //email con formato incorrecto
+        if (!username.matches(Regex("[a-zA-Z0-9]+"))) {
+            _errorMessage.value = "El usuario solo puede contener letras, números y guiones bajos"
+            return
+        }
+
         if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             _errorMessage.value = "Email no válido"
             return
         }
 
         if (password.length < 6) {
-            _errorMessage.value = "Contraseña de al menos 6 caracteres"
+            _errorMessage.value = "Contraseña demasiado corta"
             return
         }
 
@@ -53,35 +54,9 @@ class CreateAccountViewModel : ViewModel() {
             return
         }
 
-        if (username.length > 30) {
-            _errorMessage.value = "El usuario es demasiado largo"
-            return
-        }
-
-        if (nombreConNoCaracterValido(username)) {
-            _errorMessage.value = "Caracter no valido en el usuario"
-            return
-        }
-
-        if (username.contains("!") || username.contains("@")) {
-            _errorMessage.value = "El usuario es demasiado largo"
-            return
-        }
-
-
         _errorMessage.value = null
         _registerResult.value = true
 
     }
 
 }
-
-    fun nombreConNoCaracterValido(name: String): Boolean {
-        var lista: String = "!#$%&/()=?¿|ª€·-;,\"\'¬~-+*{}<>[] "
-        for (c in name) {
-            if (c in lista) {
-                return true
-            }
-        }
-        return  false
-    }
